ec6537f05a46a38a08ce74610f44147b58f0a51c
naomiuwa <naomi.uwamungu@gmail.com>
2023-04-21 12:17:39 +0100

    render images and types for each pokemon

--- pokemon-library/src/components/Card/index.js
+++ pokemon-library/src/components/Card/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-function Card() {
+function Card({ pokemon }) {
   return (
     <div>
-      <h1>card placeholder</h1>
+      <h1>{pokemon.name}</h1>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name}></img>
+      <ul>
+        {pokemon.types.map((type, index) => (
+          <li key={index}>{type.type.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
