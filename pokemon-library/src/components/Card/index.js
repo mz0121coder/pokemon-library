413b6ae6bebff2854a9171f9bbc8ff429af6476c
Jakir9 <jashraf98@hotmail.co.uk>
2023-04-21 12:36:56 +0100

    added pokemon details on list of pokemons

--- pokemon-library/src/components/Card/index.js
+++ pokemon-library/src/components/Card/index.js
@@ -3,11 +3,25 @@ import React from "react";
 function Card({ pokemon }) {
   return (
     <div>
-      <h1>{pokemon.name}</h1>
+      <h1>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h1>
+      <h2>ID: {pokemon.id}</h2>
       <img src={pokemon.sprites.front_default} alt={pokemon.name}></img>
       <ul>
+        Types:
         {pokemon.types.map((type, index) => (
           <li key={index}>{type.type.name}</li>
+        ))}
+      </ul>
+
+      <ul>
+        <li> Height:{pokemon.height}</li>
+        <li>Weight:{pokemon.weight} </li>
+      </ul>
+
+      <ul>
+        Ability:{" "}
+        {pokemon.abilities.map((ability, index) => (
+          <li key={index}>{ability.ability.name}</li>
         ))}
       </ul>
     </div>
---
+++ pokemon-library/src/components/MainCard/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function MainCard({ pokemon }) {
+  return (
+    <div>
+      <h1>Choose Your Pokemon:</h1>
+    </div>
+  );
+}
+export default MainCard;
