29db3a970c014f75be1a35f25022d285ef37f48c
Mohammad Zafar <mzcoder7@gmail.com>
2023-04-21 14:29:08 +0100

    search functionality for pokemon

--- pokemon-library/src/components/App/index.js
+++ pokemon-library/src/components/App/index.js
@@ -1,39 +1,53 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios"; // Import the axios library to make API calls
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; // Import the axios library to make API calls
 
-import Card from "../Card"; // Import the Card component
-import MainCard from "../MainCard"; // Import the PokemonList component
+import Card from '../Card'; // Import the Card component
+// import MainCard from '../MainCard'; // Import the PokemonList component
 
 function App() {
-  const [pokemonList, setPokemonList] = useState([]); // Set up state to hold the list of Pokemon
+	// create a state variable 'searchTerm'
+	const [search, setSearch] = useState('');
+	const [pokemonList, setPokemonList] = useState([]); // Set up state to hold the list of Pokemon
 
-  useEffect(() => {
-    const getPokemonList = async () => {
-      const promises = []; // Create an empty array to hold promises for each API call
-      for (let i = 1; i <= 100; i++) {
-        // Loop 100 times to get 100 random Pokemon
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`; // Generate the first 100 Pokemon's URL
-        promises.push(axios.get(url)); // Add a promise for the API call to the promises array
-      }
-      const responses = await Promise.all(promises); // Wait for all promises to resolve
-      // console.log(responses);
-      const data = responses.map((response) => response.data); // Extract the data from each response
-      console.log(data);
-      setPokemonList(data); // Set the state with the list of Pokemon data
-    };
-    getPokemonList(); // Call the function to get the list of Pokemon
-  }, []); // Only run this effect once, when the component mounts
+	useEffect(() => {
+		const getPokemonList = async () => {
+			const promises = []; // Create an empty array to hold promises for each API call
+			for (let i = 1; i <= 1000; i++) {
+				// Loop 100 times to get 100 random Pokemon
+				const url = `https://pokeapi.co/api/v2/pokemon/${i}`; // Generate the first 100 Pokemon's URL
+				promises.push(axios.get(url)); // Add a promise for the API call to the promises array
+			}
+			const responses = await Promise.all(promises); // Wait for all promises to resolve
+			// console.log(responses);
+			const data = responses.map(response => response.data); // Extract the data from each response
+			console.log(data);
+			setPokemonList(data); // Set the state with the list of Pokemon data
+		};
+		getPokemonList(); // Call the function to get the list of Pokemon
+	}, []); // Only run this effect once, when the component mounts
 
-  return (
-    <main>
-      <MainCard />
-      {/* Render the PokemonList component */}
+	// create function to update the search term whenever user types in the search input
+	const handleSearch = event => {
+		setSearch(event.target.value);
+	};
+	// filter the pokemonList to only show pokemon whose name starts with the search term
+	// 'pi' => pikachu etc
+	const filteredList = pokemonList.filter(pokemon =>
+		pokemon.name.toLowerCase().startsWith(search.toLowerCase())
+	);
 
-      {pokemonList.map((pokemon) => (
-        <Card key={pokemon.id} pokemon={pokemon} /> // Map over the list of Pokemon data and render a Card component for each one
-      ))}
-    </main>
-  );
+	return (
+		<main>
+			{/* <MainCard /> */}
+			{/* Render the PokemonList component */}
+			<label htmlFor='search'>Search:</label>
+			<input type='text' id='search' value={search} onChange={handleSearch} />
+
+			{filteredList.map(pokemon => (
+				<Card key={pokemon.id} pokemon={pokemon} /> // Map over the list of Pokemon data and render a Card component for each one
+			))}
+		</main>
+	);
 }
 
 export default App;
--- pokemon-library/src/components/Card/index.js
+++ pokemon-library/src/components/Card/index.js
@@ -1,48 +1,48 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function Card({ pokemon }) {
-  const [showDetails, setShowDetails] = useState(false);
-  console.log(showDetails);
+	const [showDetails, setShowDetails] = useState(false);
+	console.log(showDetails);
 
-  const handleClick = () => {
-    setShowDetails(!showDetails);
-    console.log(showDetails);
-  };
+	const handleClick = () => {
+		setShowDetails(!showDetails);
+		console.log(showDetails);
+	};
 
-  return (
-    <div>
-      <h1>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h1>
+	return (
+		<div>
+			<h1>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h1>
 
-      <img src={pokemon.sprites.front_default} alt={pokemon.name}></img>
+			<img src={pokemon.sprites.front_default} alt={pokemon.name}></img>
 
-      {showDetails && (
-        <>
-          <h4>ID: {pokemon.id}</h4>
-          <ul>
-            Types:
-            {pokemon.types.map((type, index) => (
-              <li key={index}>{type.type.name}</li>
-            ))}
-          </ul>
+			{showDetails && (
+				<>
+					<h4>ID: {pokemon.id}</h4>
+					<ul>
+						Types:
+						{pokemon.types.map((type, index) => (
+							<li key={index}>{type.type.name}</li>
+						))}
+					</ul>
 
-          <ul>
-            <li> Height:{pokemon.height}</li>
-            <li>Weight:{pokemon.weight} </li>
-          </ul>
+					<ul>
+						<li> Height:{pokemon.height}</li>
+						<li>Weight:{pokemon.weight} </li>
+					</ul>
 
-          <ul>
-            Ability:{" "}
-            {pokemon.abilities.map((ability, index) => (
-              <li key={index}>{ability.ability.name}</li>
-            ))}
-          </ul>
-        </>
-      )}
+					<ul>
+						Abilities:{' '}
+						{pokemon.abilities.map((ability, index) => (
+							<li key={index}>{ability.ability.name}</li>
+						))}
+					</ul>
+				</>
+			)}
 
-      <button onClick={handleClick}>
-        {showDetails ? "hide details" : "view details"}
-      </button>
-    </div>
-  );
+			<button onClick={handleClick}>
+				{showDetails ? 'hide details' : 'view details'}
+			</button>
+		</div>
+	);
 }
 export default Card;
