5562c2422a1ab275fbdc195cc41427ccc2965ee1
Jakir9 <jashraf98@hotmail.co.uk>
2023-04-21 10:24:34 +0100

    added skeleton code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,1 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Card from "../Card";
+import PokemonList from "../PokemonList";
+
+function App() {
+  const [pokemon, setPokemon] = useState({}); //set as empty pokemon object
+
+  return (
+    <main>
+      <Card pokemon={pokemon} />
+      <PokemonList />
+    </main>
+  );
+}
+
+export default App;
