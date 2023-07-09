5e2f0a178488753a0908e7328e4ce37ae371eba7
Mohammad Zafar <mzcoder7@gmail.com>
2023-07-10 00:24:02 +0100

    Revert "add comments to Card component"
    This reverts commit f0722503dfc70c2ee5a40e573c2be60889e18fca.

--- pokemon-library/src/index.js
+++ pokemon-library/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import './index.css';
 import App from '../src/components/App/index.js';
 import reportWebVitals from './reportWebVitals';
 
